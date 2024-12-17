bc0463b0abf68b9ba629e39afa7bd3dfffe698c4
aleme79 <alemetroid770@gmail.com>
2024-12-17 16:28:24 +0100

    Update PruebaProgramacion.java

--- prueba/src/prueba2/PruebaProgramacion.java
+++ prueba/src/prueba2/PruebaProgramacion.java
@@ -4,7 +4,7 @@ public class PruebaProgramacion {
 
 	public static void main(String[] args) {
 		System.out.println("Holaaa esto es una prueba");
-		
+		//comentario añadido
 		System.out.println("Adiooooos a esta prueba");
 	}
 
