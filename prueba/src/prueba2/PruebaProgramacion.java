1e747f1ab3ba139553f2b68f2278d86cd71e1134
aleme79 <alemetroid770@gmail.com>
2024-12-17 16:21:53 +0100

    Edit del proyecto

--- prueba/src/prueba2/PruebaProgramacion.java
+++ prueba/src/prueba2/PruebaProgramacion.java
@@ -4,6 +4,8 @@ public class PruebaProgramacion {
 
 	public static void main(String[] args) {
 		System.out.println("Holaaa esto es una prueba");
+		
+		System.out.println("Adiooooos a esta prueba");
 	}
 
 }
